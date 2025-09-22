80709d1725c484a246de0fef8ba1ef0f4ab4f5bd
Rocío Staltari <rostalbon@gmail.com>
2025-09-22 13:07:07 -0300

    Se modificó el main

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-#aoaooaoa
+from cocinero import Cocinero
+
+titulo_programa = "ROBOT"
+print(f"╔═{"═" * len(titulo_programa)}═╗")
+print(f"║ {" " * len(titulo_programa)} ║")
+print(f"║ {titulo_programa} ║")
+print(f"║ {" " * len(titulo_programa)} ║")
+print(f"╚═{"═" * len(titulo_programa)}═╝")
