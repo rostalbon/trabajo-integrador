4c06525ee69f1268559ca3bda88fb8cae64698e8
Rocío Staltari <rostalbon@gmail.com>
2025-09-22 09:59:51 -0300

    se modificó la clase Robot

--- robot.py
+++ robot.py
@@ -1,15 +1,18 @@
 class Robot:
-    def __init__(self, nombre, color, microfono=0, parlante=0):
+    def __init__(self, nombre, color, microfono=0, parlante=0, onOff=0):
         self._color = color
         self._microfono = microfono
         self._parlante = parlante
         self._nombre = nombre
+        self._onOff = onOff
 
     def encender(self):
-        return f"{self._nombre} esta encendido"
+        self._onOff = 1
+        return f"{self._nombre} está encendido"
 
     def apagar(self):
-        return f"{self._nombre} esta apagado"
+        self._onOff = 0
+        return f"{self._nombre} está apagado"
 
     def mover(self):
-        return f"{self._nombre} se movio correctamente"
+        return f"{self._nombre} se movió correctamente"
