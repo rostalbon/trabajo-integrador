b42cf949442d358815b631cc4fa227b6c3e9df6e
liwen <albrechtliwen1@gmail.com>
2025-09-23 13:42:30 -0300

    se arregló la clase niñero

--- niñero.py
+++ niñero.py
@@ -3,9 +3,22 @@ from robot import Robot
 class niñero(Robot):
     def __init__(self, nombre, color, mamadera=False, articulosLimpieza=False, microfono=True, parlante=True, onOff=False):
         super().__init__(nombre, color, microfono, parlante, onOff)
-        self._mamadera=mamadera
-        self._articulosLimpieza=articulosLimpieza
-
+        self._mamadera = mamadera
+        self._articulosLimpieza = articulosLimpieza
+    
     def limpiar(self):
         if self._articulosLimpieza==True:
             print("El robot está limpiando")
+        else:
+            print("¡El robot no tiene artículos de limpieza! Va a buscarlos.")
+            self._articulosLimpieza=True
+    
+    def darMamadera(self):
+        if self._mamadera==True:
+            print("El robot está dandole la mamadera al niño.")
+        else:
+            print("¡El robot no encuentra la mamadera! Va a buscarla.")
+            self._mamadera=True
+    
+    def arropar(self):
+        print("El robot arropó al niño.")
