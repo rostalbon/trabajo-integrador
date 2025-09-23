b49a4a921ac4833fd1e3b8d8e7c15b43a9ab1fa4
Angeles Pietra <angeles10pietra@gmail.com>
2025-09-22 21:58:58 -0300

    Se agregaron las acciones del carpintero

--- versiones/carpintero.py
+++ versiones/carpintero.py
@@ -7,4 +7,42 @@ class Carpintero(Robot):
         self._sierra = sierra
         self._destornillador = destornillador
 
-   
+    def hachar(self):
+        if not self._onOff:
+                return f"{self._nombre} está apagado. Enciéndelo primero."
+        if self._hacha:
+            self._hacha = False
+            return f"{self._nombre} dejó de usar el hacha"
+        else:
+            self._hacha = True
+            return f"{self._nombre} está hachando madera"
+
+    def cerruchar(self):
+        if not self._onOff:
+            return f"{self._nombre} está apagado. Enciéndelo primero."
+        if self._sierra:
+            self._sierra = False
+            return f"{self._nombre} dejó de usar la sierra"
+        else:
+            self._sierra = True
+            return f"{self._nombre} está cerruchando madera"
+
+    def atornillar(self):
+        if not self._onOff:
+            return f"{self._nombre} está apagado. Enciéndelo primero."
+        if self._destornillador:
+            self._destornillador = False
+            return f"{self._nombre} dejó de atornillar"
+        else:
+            self._destornillador = True
+            return f"{self._nombre} está atornillando"
+
+    def desatornillar(self):
+        if not self._onOff:
+            return f"{self._nombre} está apagado. Enciéndelo primero."
+        if self._destornillador:
+            self._destornillador = False
+            return f"{self._nombre} dejó de desatornillar"
+        else:
+            self._destornillador = True
+            return f"{self._nombre} está desatornillando"
