d2c745fc8749d4824eb7c0af70f1ed94e067f15d
Nahiara Millahuala <nahi042007@gmail.com>
2025-09-23 08:38:34 -0300

    Cambio de mensaje en el método barrer

--- versiones/deLimpieza.py
+++ versiones/deLimpieza.py
@@ -1,17 +1,18 @@
 from robot import Robot
 
 class robotLimpieza(Robot):
-    def __init__(self, nombre, color, microfono=True, parlante=True, onOff=False, escoba=False, aspiradora=False, trapeadora=False):
+    def __init__(self, nombre, color, microfono=True, parlante=True, onOff=False, escoba=True, aspiradora=True, trapeadora=True):
         super().__init__(nombre, color, microfono, parlante, onOff)
         self._escoba = escoba
         self._aspiradora = aspiradora
         self._trapeadora = trapeadora
     
     def barrer(self):
-        if self._escoba:
-            return f"{self._nombre} está barriendo"
+        if self._escoba == False:
+            self._escoba = True
+            return f"{self._nombre} dejo de barrer"
         else:
-            return f"{self._nombre} no cuenta con la escoba disponible"
+            return f"{self._nombre} está barriendo"
         
     def aspirar(self):
         if self._aspiradora:
