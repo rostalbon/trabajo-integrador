08483362626ce00c6d00ddb2ac293d76ca4d28a9
Rocío Staltari <rostalbon@gmail.com>
2025-09-25 11:26:19 -0300

    Se modificó la clase Bateria

--- bateria.py
+++ bateria.py
@@ -1,3 +1,8 @@
 class Bateria:
     def __init__(self, porcentaje=100):
         self._porcentaje = porcentaje
+    def get_porcentaje(self):
+        return self._porcentaje
+    def set_porcentaje(self, porcentaje):
+        self._porcentaje = porcentaje
+        return f"El porcentaje de batería actual es {self._pocentaje}%"
