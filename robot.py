f9b6b169f1ea4bf2f255db29ee13626dae7569d9
Rocío Staltari <rostalbon@gmail.com>
2025-09-25 11:37:53 -0300

    Se incorporó la batería como agregación en robot

--- robot.py
+++ robot.py
@@ -1,10 +1,20 @@
+from bateria import Bateria
+
 class Robot:
-    def __init__(self, nombre, color, microfono=True, parlante=True, onOff=False):
+    def __init__(self, nombre, color, bateria=100, microfono=True, parlante=True, onOff=False):
         self._color = color
         self._microfono = microfono
         self._parlante = parlante
         self._nombre = nombre
         self._onOff = onOff
+        self._bateria = Bateria(bateria)
+
+    def get_bateria(self):
+        return self._bateria
+
+    def set_bateria(self, bateria):
+        self._bateria = Bateria(bateria)
+        return f"Se actualizó el porcentaje de batería a {self._bateria}%"
 
     def get_nombre(self):
         return self._nombre
